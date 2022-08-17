6a27c5c463f76f90cf668d47e281c928e33b671d
James Mburu Githire <jamesmburug24@gmail.com>
2022-08-17 12:25:39 +0300

    Completed assignment with 1 stretched deliverable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  const description = document.getElementById("new-task-description");
+  const list = document.getElementById("tasks");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const delB = document.createElement("button");
+    delB.textContent = "X";
+    const val = description.value;
+    delB.addEventListener("click", (e) => {
+      e.target.parentNode.remove();
+    })
+    delB.style="left:20px"
+    const newLi = document.createElement("li");
+    newLi.textContent = val + "  ";
+    newLi.appendChild(delB);
+    list.appendChild(newLi);
+    description.value = "";
+
+  })
+  e.preventDefault();
+  const ulist = document.getElementById("tasks");
+
 });
